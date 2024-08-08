9961ff5d6914175f8c1b4e3193ceba63d77d75db
Kevin Ciardelli <ciardelk@bc.edu>
2024-08-08 13:09:54 -0700

    Add domain

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
