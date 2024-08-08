f2df13778ac9bb44f182dadca1b610fdba2990b0
Kevin Ciardelli <ciardelk@bc.edu>
2024-08-08 12:50:35 -0700

    Add domain

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>here</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+        <Route exact path="/" component={Home} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
