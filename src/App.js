9c3615ec6f42ac3d8dc14fce9f03fbc1473d24c2
Kevin Ciardelli <ciardelk@bc.edu>
2024-08-08 14:09:14 -0700

    Finishing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import About from './components/pages/About'
 import Contact from './components/pages/Contact'
 import Gallery from './components/pages/Gallery'
 import PoolHouse from './components/pages/PoolHouse'
+import Woodwork from './components/pages/Woodwork'
+import Cabana from './components/pages/Cabana'
+import OakDrive from './components/pages/OakDrive'
+import MasterBathroom from './components/pages/MasterBathroom'
+import Exterior from './components/pages/Exterior'
 
 function App() {
   return (
@@ -19,7 +24,12 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Gallery" element={<Gallery />} />
-        <Route path="/PoolHouse" element={<PoolHouse />} />          
+        <Route path="/PoolHouse" element={<PoolHouse />} />   
+        <Route path="/Woodwork" element={<Woodwork />} />
+        <Route path="/Cabana" element={<Cabana />} />
+        <Route path="/OakDrive" element={<OakDrive />} />
+        <Route path="/MasterBathroom" element={<MasterBathroom />} />
+        <Route path="/Exterior" element={<Exterior />} />       
 
       </Routes>
       <Footer />
